bccd398cd4e7e4e6e7f5ac85a84e6b65a9df9d29
katerynapidhorna <katerynapidhorna@gmail.com>
2020-09-09 12:22:05 +0200

    changing nav position using jQuery

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-$(document).ready(() => {
-  console.log("Ready!");
+$(() => {
+  const nav = $(".nav-list");
+  let lastScrollTop = 0;
+  const $w = $(window);
+  if ($w.scrollTop() >= 698) {
+    nav.css({
+      top: `40px`,
+      position: "fixed",
+    });
+  }
+  // TODO: throttle scroll callback
+  $w.on("scroll", () => {
+    const currentScrollTop = $w.scrollTop();
+    const direction = lastScrollTop < currentScrollTop ? "down" : "up";
+    lastScrollTop = currentScrollTop;
+    if (direction === "down" && lastScrollTop >= 650) {
+      nav.css({
+        top: "40px",
+        position: "fixed",
+      });
+    }
+    if (direction === "up") {
+      if (nav.offset().top <= $(".profile-container").offset().top) {
+        nav.css({
+          top: "0",
+          position: "absolute",
+        });
+      }
+    }
+  });
 });
