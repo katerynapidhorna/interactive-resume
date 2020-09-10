9502c2311500859cf53a8beb2ded5e78a4b4ebf0
katerynapidhorna <katerynapidhorna@gmail.com>
2020-09-10 10:20:43 +0200

    fix behaviour of nav list depending width, resize and scroll

--- index.js
+++ index.js
@@ -1,31 +1,88 @@
 $(() => {
+  const $w = $(window);
+
+  const doSomething = () => {
+    if ($w.width() <= 600) {
+      return;
+    } else {
+      const currentScrollTop = $w.scrollTop();
+      const direction = lastScrollTop < currentScrollTop ? "down" : "up";
+      lastScrollTop = currentScrollTop;
+      if (direction === "down" && lastScrollTop >= 650) {
+        nav.css({
+          top: "40px",
+          position: "fixed",
+        });
+      }
+      if (direction === "up") {
+        if (nav.offset().top <= $(".profile-container").offset().top) {
+          nav.css({
+            top: "0",
+            position: "absolute",
+          });
+        }
+      }
+    }
+  };
+
+  //add/remove classes depending on screen width_______START
+  if ($w.width() <= 600) {
+    $("nav ul").removeClass("nav-list");
+    $("nav ul").addClass("media-nav-list");
+  } else {
+    $("nav ul").removeClass("media-nav-list");
+    $("nav ul").addClass("nav-list");
+  }
+
+  $w.on("resize", () => {
+    if ($w.width() <= 600) {
+      $("nav ul").removeClass("nav-list");
+      $("nav ul").addClass("media-nav-list");
+    } else {
+      $("nav ul").removeClass("media-nav-list");
+      $("nav ul").addClass("nav-list");
+    }
+  });
+  //add/remove classes depending on screen width_______END
+
+  //changing position of the element on scroll_______START
   const nav = $(".nav-list");
   let lastScrollTop = 0;
-  const $w = $(window);
   if ($w.scrollTop() >= 698) {
     nav.css({
       top: `40px`,
       position: "fixed",
     });
   }
-  // TODO: throttle scroll callback
+
   $w.on("scroll", () => {
-    const currentScrollTop = $w.scrollTop();
-    const direction = lastScrollTop < currentScrollTop ? "down" : "up";
-    lastScrollTop = currentScrollTop;
-    if (direction === "down" && lastScrollTop >= 650) {
+    if ($w.width() <= 600) {
       nav.css({
-        top: "40px",
-        position: "fixed",
+        top: "0",
+        position: "absolute",
       });
-    }
-    if (direction === "up") {
-      if (nav.offset().top <= $(".profile-container").offset().top) {
+      return;
+    } else {
+      const currentScrollTop = $w.scrollTop();
+      const direction = lastScrollTop < currentScrollTop ? "down" : "up";
+      lastScrollTop = currentScrollTop;
+      if (direction === "down" && lastScrollTop >= 650) {
         nav.css({
-          top: "0",
-          position: "absolute",
+          top: "40px",
+          position: "fixed",
         });
+      }
+      if (direction === "up") {
+        if (nav.offset().top <= $(".profile-container").offset().top) {
+          nav.css({
+            top: "0",
+            position: "absolute",
+          });
+        }
       }
     }
   });
+  //changing position of the element on scroll_______END
 });
+
+// TODO: throttle scroll callback
