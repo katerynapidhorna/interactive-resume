f3be76f1e9530527f56a5296d7cee6a9527ba7b8
katerynapidhorna <katerynapidhorna@gmail.com>
2020-09-10 11:40:46 +0200

    fix behaviour of nav list depending width, resize and scroll

--- index.js
+++ index.js
@@ -1,83 +1,80 @@
 $(() => {
   const $w = $(window);
+  const nav = $(".nav-list");
+  let lastScrollTop = 0;
 
-  const doSomething = () => {
-    if ($w.width() <= 600) {
-      return;
-    } else {
-      const currentScrollTop = $w.scrollTop();
-      const direction = lastScrollTop < currentScrollTop ? "down" : "up";
-      lastScrollTop = currentScrollTop;
-      if (direction === "down" && lastScrollTop >= 650) {
-        nav.css({
-          top: "40px",
-          position: "fixed",
-        });
-      }
-      if (direction === "up") {
-        if (nav.offset().top <= $(".profile-container").offset().top) {
-          nav.css({
-            top: "0",
-            position: "absolute",
-          });
+  const addFixed = () => {
+    const nav = $(".nav-list");
+    nav.css({
+      top: `40px`,
+      position: "fixed",
+    });
+  };
+  const addAbsolute = () => {
+    const nav = $(".nav-list");
+    nav.css({
+      top: "0",
+      position: "absolute",
+    });
+  };
+
+  const checkOnScroll = () => {
+    $w.on("scroll", () => {
+      if ($w.width() <= 600) {
+        addAbsolute();
+        return;
+      } else {
+        const currentScrollTop = $w.scrollTop();
+        const direction = lastScrollTop < currentScrollTop ? "down" : "up";
+        lastScrollTop = currentScrollTop;
+        if (direction === "down" && lastScrollTop >= 650) {
+          addFixed();
+        }
+        if (direction === "up") {
+          if (nav.offset().top <= $(".profile-container").offset().top) {
+            addAbsolute();
+          }
         }
       }
-    }
+    });
   };
 
   //add/remove classes depending on screen width_______START
   if ($w.width() <= 600) {
     $("nav ul").removeClass("nav-list");
     $("nav ul").addClass("media-nav-list");
-  } else {
-    $("nav ul").removeClass("media-nav-list");
-    $("nav ul").addClass("nav-list");
   }
 
   $w.on("resize", () => {
     if ($w.width() <= 600) {
-      $("nav ul").removeClass("nav-list");
-      $("nav ul").addClass("media-nav-list");
+      $("nav ul").removeClass("nav-list").addClass("media-nav-list");
+      return;
     } else {
-      $("nav ul").removeClass("media-nav-list");
-      $("nav ul").addClass("nav-list");
+      $("nav ul").removeClass("media-nav-list").addClass("nav-list");
+      checkOnScroll();
     }
   });
   //add/remove classes depending on screen width_______END
 
   //changing position of the element on scroll_______START
-  const nav = $(".nav-list");
-  let lastScrollTop = 0;
-  if ($w.scrollTop() >= 698) {
-    nav.css({
-      top: `40px`,
-      position: "fixed",
-    });
+  if ($w.scrollTop() >= 698 && $w.width() > 600) {
+    addFixed();
   }
 
   $w.on("scroll", () => {
     if ($w.width() <= 600) {
-      nav.css({
-        top: "0",
-        position: "absolute",
-      });
+      addAbsolute();
       return;
     } else {
       const currentScrollTop = $w.scrollTop();
       const direction = lastScrollTop < currentScrollTop ? "down" : "up";
       lastScrollTop = currentScrollTop;
       if (direction === "down" && lastScrollTop >= 650) {
-        nav.css({
-          top: "40px",
-          position: "fixed",
-        });
+        addFixed();
       }
       if (direction === "up") {
         if (nav.offset().top <= $(".profile-container").offset().top) {
-          nav.css({
-            top: "0",
-            position: "absolute",
-          });
+          addAbsolute();
         }
       }
     }
